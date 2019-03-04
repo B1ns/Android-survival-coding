ffed2d12c0d8c76b874c2cf463b6baa1b0a9f058
정빈 <qlsl3574@naver.com>
2019-03-04 19:56:21 +0900

    MyGallery - 전자액자 구현중

---
+++ app/src/main/java/com/example/androidsurvivalcoding/MyGallery.kt
@@ -0,0 +1,36 @@
+package com.example.androidsurvivalcoding
+
+import android.Manifest
+import android.content.pm.PackageManager
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.provider.MediaStore
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
+import org.jetbrains.anko.alert
+
+class MyGallery : AppCompatActivity() {
+
+    private val REQUEST_READ_EXTERNAL_STORAGE = 1000
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_my_gallery)
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
+            if(ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.READ_EXTERNAL_STORAGE)){
+                alert("사진 정보를 얻으려면 외부 저장소 권한이 필요함")
+           }
+        }
+
+        getAllPhotos()
+
+    }
+
+    private fun getAllPhotos() {
+        val cursor = contentResolver.query(
+            MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+            null, null, null, MediaStore.Images.ImageColumns.DATE_TAKEN + "DESC" // 찍은 날짜 내림차순
+        )
+    }
+}
