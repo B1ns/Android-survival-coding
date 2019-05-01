109c6cfd19c9be9f7e174b2504eb83461a6dc7a2
B1ns <qlsl3574@naver.com>
2019-05-01 11:11:48 +0900

    new file

--- app/src/main/java/com/example/androidsurvivalcoding/Todo/TodoActivity.kt
+++ app/src/main/java/com/example/androidsurvivalcoding/Todo/TodoActivity.kt
@@ -1,8 +1,9 @@
-package com.example.androidsurvivalcoding
+package com.example.androidsurvivalcoding.Todo
 
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity;
+import com.example.androidsurvivalcoding.R
 
 import kotlinx.android.synthetic.main.activity_todo.*
 
