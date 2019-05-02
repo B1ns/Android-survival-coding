db6eecf4c03dba4ba3b23e9f71d70aa2384837b6
B1ns <qlsl3574@naver.com>
2019-05-03 02:51:51 +0900

    Todo

--- app/src/main/java/com/example/androidsurvivalcoding/Todo/EditActivity.kt
+++ app/src/main/java/com/example/androidsurvivalcoding/Todo/EditActivity.kt
@@ -3,11 +3,53 @@ package com.example.androidsurvivalcoding.Todo
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import com.example.androidsurvivalcoding.R
+import io.realm.Realm
+import io.realm.kotlin.createObject
+import io.realm.kotlin.where
+import kotlinx.android.synthetic.main.activity_edit.*
+import org.jetbrains.anko.alert
+import org.jetbrains.anko.yesButton
+import java.util.*
 
 class EditActivity : AppCompatActivity() {
+
+    val calendar: Calendar = Calendar.getInstance()
+
+    val realm = Realm.getDefaultInstance() // 날짜를 다룰 캘린더 객체
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit)
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        realm.close()
+    }
+
+    private fun insertTodo() {
+        realm.beginTransaction() // 트랜잭션 시작
+
+        val newItem = realm.createObject<Todo>(nextId()) // 새 객체 생성
+        // 값 설정
+        newItem.title = todoEditText.text.toString()
+        newItem.date = calendar.timeInMillis
+
+        realm.commitTransaction() // 트랜잭션 종료 반영
+
+        // 다이얼로그 표시
+        alert("내용이 추가되었습니다.") {
+            yesButton { finish() }
+        }.show()
+    }
+
+    //다음 id 를 반환
+    private fun nextId(): Int {
+        val maxId = realm.where<Todo>().max("id")
+        if (maxId != null) {
+            return maxId.toInt() + 1
+        }
+        return 0
     }
 }
---
+++ app/src/main/java/com/example/androidsurvivalcoding/Todo/MyApplication.kt
@@ -0,0 +1,11 @@
+package com.example.androidsurvivalcoding.Todo
+
+import android.app.Application
+import io.realm.Realm
+
+class MyApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        Realm.init(this)
+    }
+}
